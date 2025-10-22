1357ef9c1772396c907349e6c5b8945375c1b529
suramya-biswas-git <suramya.biswas@gmail.com>
2025-10-22 20:13:55 +0530

    MyApp 2nd Version

--- MyApp/src/main/java/org/demo/MyApp/App.java
+++ MyApp/src/main/java/org/demo/MyApp/App.java
@@ -6,5 +6,6 @@ package org.demo.MyApp;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Happy Diwali");
     }
 }
---
+++ MyApp/src/main/java/org/demo/MyApp/Welcome.java
@@ -0,0 +1,10 @@
+package org.demo.MyApp;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome To GITHUB Demo");
+
+	}
+
+}
